4f5fbd1ff000df3c4c558525f237b5183afa2199
doaingworth <69863980+doaingworth@users.noreply.github.com>
2020-08-18 09:37:23 -0700

    Create FourFours.java

---
+++ FourFours.java
@@ -0,0 +1,14 @@
+public class Four4s{
+  public static void main(String[] args){
+    System.out.println(44/44);
+    System.out.println((4/4)+(4/4));
+    System.out.println((4+4+4)/4);
+    System.out.println(4*(4-4)+4);
+    System.out.println(((4*4)+4)/4);
+    System.out.println(((4 + 4) / 4) + 4);
+    System.out.println(4 + 4 - (4 / 4));
+    System.out.println(4 + 4 + 4 - 4);
+    System.out.println(4 + 4 + (4 / 4));
+    System.out.println((44 - 4) / 4);
+  }
+}
